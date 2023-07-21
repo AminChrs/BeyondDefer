a684d0142d03de22091ddc7ca79798ebfbd9fca1
Amin <mcharusaie@tuebingen.mpg.de>
2023-07-21 20:47:55 +0200

    comb_losss

--- misc/losses.py
+++ misc/losses.py
@@ -37,6 +37,27 @@ class Criterion(object):
 		l = m2 * (l1 + l2) + l3 + l5
 
 		return torch.mean(l)
+	def comb_ova(self, outputs_classifier, outputs_sim, outputs_meta, m, labels, n_classes):
+		'''
+		The L_{CE} loss implementation for CIFAR
+		----
+		outputs: network outputs
+		m: human label
+		labels: target
+		outputs_classifier: output of the classifier
+		outputs_sim: output of the human simulator
+		outputs_meta: output of the meta classifier
+		n_classes: number of classes
+		'''
+		batch_size = outputs_classifier.size()[0]  # batch_size
+		l1 = Criterion.LogisticLoss(outputs_classifier[range(batch_size), labels], 1)
+		l2 = torch.sum(Criterion.LogisticLoss(outputs_classifier[:,:n_classes], -1), dim=1) - Criterion.LogisticLoss(outputs_classifier[range(batch_size),labels],-1)
+		l3 = Criterion.LogisticLoss(outputs_meta[range(batch_size), labels], 1)
+		l4 = torch.sum(Criterion.LogisticLoss(outputs_meta[:,:n_classes], -1), dim=1) - Criterion.LogisticLoss(outputs_meta[range(batch_size),labels],-1)
+		l5 = Criterion.LogisticLoss(outputs_sim[range(batch_size), m], 1)
+		l6 = torch.sum(Criterion.LogisticLoss(outputs_sim[:,:n_classes], -1), dim=1) - Criterion.LogisticLoss(outputs_sim[range(batch_size),m],-1)
+		l = l1 + l2 + l3 + l4 + l5 + l6
+		return torch.mean(l)
 
 	@staticmethod
 	def LogisticLoss(outputs, y):

46c0126a5f557311416cfff71dd40340051a31ce
Amirmehdi Jafari Fesharaki <Amiroo23jf@gmail.com>
2023-09-23 17:41:57 +0330

    add realizable surrogate cost sensitive

--- MyMethod/costy_defer.py
+++ MyMethod/costy_defer.py
@@ -23,6 +23,8 @@ from human_ai_deferral.helpers.metrics import *
 from human_ai_deferral.baselines.basemethod import BaseMethod, BaseSurrogateMethod
 from human_ai_deferral.baselines.compare_confidence import CompareConfidence
 from human_ai_deferral.baselines.one_v_all import OVASurrogate
+from human_ai_deferral.baselines.lce_surrogate import LceSurrogate
+from human_ai_deferral.methods.realizable_surrogate import RealizableSurrogate
 
 
             
@@ -412,5 +414,166 @@ class LceSurrogateCostSensitive(LceSurrogate):
         # TODO: COMPLETE THIS
         
         pass
+    
+    
+ 
+class RealizableSurrogateCostSensitive(RealizableSurrogate):
+    def test(self, dataloader, loss_fn):
+        """
+        Test the model
+        dataloader: dataloader
+        """
+        defers_all = []
+        truths_all = []
+        hum_preds_all = []
+        predictions_all = []  # classifier only
+        rej_score_all = []  # rejector probability
+        class_probs_all = []  # classifier probability
+        self.model.eval()
+        with torch.no_grad():
+            for batch, (data_x, data_y, hum_preds) in enumerate(dataloader):
+                data_x = data_x.to(self.device)
+                data_y = data_y.to(self.device)
+                hum_preds = hum_preds.to(self.device)
+
+                outputs = self.model(data_x)
+                outputs_class = F.softmax(outputs[:, :-1], dim=1)
+                outputs = F.softmax(outputs, dim=1)
+                _, predicted = torch.max(outputs.data, 1)
+                max_probs, predicted_class = torch.max(outputs.data[:, :-1], 1)
+                predictions_all.extend(predicted_class.cpu().numpy())
+                
+                # TODO: Rewrite defer_binary
+                # defer_scores = [ outputs.data[i][-1].item() - outputs.data[i][predicted_class[i]].item() for i in range(len(outputs.data))]
+                # defer_binary = [int(defer_score >= self.threshold_rej) for defer_score in defer_scores]
+                defer_binary = [self.is_defer(outputs.data[i], loss_fn) for i in range(len(outputs.data))]
+                defers_all.extend(defer_binary)
+                truths_all.extend(data_y.cpu().numpy())
+                hum_preds_all.extend(hum_preds.cpu().numpy())
+                for i in range(len(outputs.data)):
+                    rej_score_all.append(
+                        outputs.data[i][-1].item()
+                        - outputs.data[i][predicted_class[i]].item()
+                    )
+                class_probs_all.extend(outputs_class.cpu().numpy())
+
+        # convert to numpy
+        defers_all = np.array(defers_all)
+        truths_all = np.array(truths_all)
+        hum_preds_all = np.array(hum_preds_all)
+        predictions_all = np.array(predictions_all)
+        rej_score_all = np.array(rej_score_all)
+        class_probs_all = np.array(class_probs_all)
+        data = {
+            "defers": defers_all,
+            "labels": truths_all,
+            "hum_preds": hum_preds_all,
+            "preds": predictions_all,
+            "rej_score": rej_score_all,
+            "class_probs": class_probs_all,
+        }
+        return data
+    
+    def fit(
+        self,
+        dataloader_train,
+        dataloader_val,
+        dataloader_test,
+        epochs,
+        optimizer,
+        lr,
+        loss_fn,
+        scheduler=None,
+        verbose=True,
+        test_interval=5,
+    ):
+        optimizer = optimizer(self.model.parameters(), lr=lr)
+        if scheduler is not None:
+            scheduler = scheduler(optimizer, len(dataloader_train) * epochs)
+        best_acc = 0
+        # store current model dict
+        best_model = copy.deepcopy(self.model.state_dict())
+        for epoch in tqdm(range(epochs)):
+            self.fit_epoch(dataloader_train, optimizer, verbose, epoch)
+            if epoch % test_interval == 0 and epoch > 1 :
+                if self.learnable_threshold_rej:
+                    self.fit_treshold_rej(dataloader_val)
+                data_test = self.test(dataloader_val, loss_fn)
+                val_metrics = compute_deferral_metrics_cost_sensitive(data_test, loss_fn)
+                if val_metrics["system_acc"] >= best_acc:
+                    best_acc = val_metrics["system_acc"]
+                    best_model = copy.deepcopy(self.model.state_dict())
+                if verbose:
+                    logging.info(compute_deferral_metrics_cost_sensitive(data_test, loss_fn))
+            if scheduler is not None:
+                scheduler.step()
+        self.model.load_state_dict(best_model)
+        if self.learnable_threshold_rej:
+            self.fit_treshold_rej(dataloader_val)
+        final_test = self.test(dataloader_test, loss_fn)
+        return compute_deferral_metrics_cost_sensitive(final_test, loss_fn)
+    
+    # fit with hyperparameter tuning over alpha
+    def fit_hyperparam(
+        self,
+        dataloader_train,
+        dataloader_val,
+        dataloader_test,
+        epochs,
+        optimizer,
+        lr,
+        loss_fn,
+        verbose=True,
+        test_interval=5,
+        scheduler=None,
+        alpha_grid=[0, 0.1, 0.3, 0.5, 0.9, 1],
+    ):
+        # np.linspace(0,1,11)
+        best_alpha = 0
+        best_acc = 0
+        model_dict = copy.deepcopy(self.model.state_dict())
+        for alpha in tqdm(alpha_grid):
+            self.alpha = alpha
+            self.model.load_state_dict(model_dict)
+            self.fit(
+                dataloader_train,
+                dataloader_val,
+                dataloader_test,
+                epochs = epochs,
+                optimizer = optimizer,
+                lr = lr,
+                loss_fn = loss_fn,
+                verbose = verbose,
+                test_interval = test_interval,
+                scheduler = scheduler,
+            )["system_acc"]
+            accuracy = compute_deferral_metrics_cost_sensitive(self.test(dataloader_val, loss_fn), loss_fn)["system_acc"]
+            logging.info(f"alpha: {alpha}, accuracy: {accuracy}")
+            if accuracy > best_acc:
+                best_acc = accuracy
+                best_alpha = alpha
+        self.alpha = best_alpha
+        self.model.load_state_dict(model_dict)
+        fit = self.fit(
+                dataloader_train,
+                dataloader_val,
+                dataloader_test,
+                epochs = epochs,
+                optimizer = optimizer,
+                lr = lr,
+                loss_fn = loss_fn,
+                verbose = verbose,
+                test_interval = test_interval,
+                scheduler = scheduler,
+            )
+        test_metrics = compute_deferral_metrics_cost_sensitive(self.test(dataloader_test, loss_fn), loss_fn)
+        return test_metrics
+    
+    def is_defer(self, outputs, loss_fn):
+        # TODO: COMPLETE THIS
+        
+        pass
+    
+    
 
             

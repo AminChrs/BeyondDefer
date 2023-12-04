43aba9fed8233bc682bf62699cb8ab10e92099ef
Amirmehdi Jafari Fesharaki <Amiroo23jf@gmail.com>
2023-12-04 20:35:42 +0330

    Removing extras, changing main to __main__

--- beyonddefer/__main__.py
+++ beyonddefer/__main__.py
@@ -1,9 +1,19 @@
+import sys
+import beyonddefer
+from beyonddefer.Tests.test import test_all
 # from beyonddefer.Experiments.SampleComp import SampleComp_par
 # from beyonddefer.Experiments.CIFAR10K import Exp_parallel
 # from beyonddefer.Experiments.no_loss_acc_cov import cov_acc_parallel
 # from beyonddefer.Experiments.acc_vs_c import acc_c_parallel
-from beyonddefer.Tests.test import test_all
-test_all()
+
+
+def main():
+    """Main Function"""
+    test_all()
+
+
+if __name__ == '__main__':
+    main()
 # iter = int(sys.argv[1])
 # Exp_parallel(array[iter])
 # cov_acc_parallel(array[iter])
